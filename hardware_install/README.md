5835e966e12e769a3df11905941b562713f3267d
aldo1m <aldo1mircea@gmail.com>
2020-11-06 21:49:41 +0200

    Sarted to work an a installatin scrip

---
+++ hardware_install/README.md
@@ -0,0 +1,14 @@
+## Bash script to automatically configure a system 
+
+### Installation 
+ ```
+git clone http://github.com/aldo-vim/aldovim_dev_env.git
+cd aldovim_dev_env/hardware_install/
+./install.sh
+```
+
+
+### TODO
+- [ ] Test it in a docker image 
+- [ ] Use github action for CICD
+
---
+++ hardware_install/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+###
+#  This script will configure bspwm on a lapopt/workstation/VM
+###
+
+
+### Install Xorg
+# Upgrade & Install
+   dnf -y upgrade && \
+
+# Make dnf to install documentation 
+   dnf -y install --setopt=tsflags='' \
+   xorg-x11-server-Xorg \
+   xorg-x11-server-common \
+   xorg-x11-utils  \
+   xorg-x11-xinit  \
+   xorg-x11-font-utils \
+   xorg-x11-fonts-100dpi \
+   bspwm \
+   sxhkd \
+   rxvt-unicode
+
+
+### Configure bspwm
+  mkdir -p  $HOME/.config/{bspwm,sxhkd} 
+
+# Copy bspwmrc
+cp  ../bspwm/bspwmrc /root/.config/bspwm/ 
+
+# Copy sxhkdrc
+cp ../bspwm/sxhkdrc /root/.config/sxhkd/ 
+
+## Copy .xinirc
+#cp ../bspwm/xinitrc /root/.xinitrc
